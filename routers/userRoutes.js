f5168ae85bf52a5301cc5ab90aeefdb502c95c38
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2024-11-24 00:14:34 +0600

    added jwt based middleware to verify user

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const userRoute = express();
 const userSchema = require("../schemas/user_schema");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
+const verify_jwt = require("../middlewares/verify_jwt");
+require("dotenv").config();
 const User = mongoose.model("User", userSchema);
 
 userRoute.get("/:id", (req, res) => {});
@@ -42,73 +44,104 @@ userRoute.post("/login", async (req, res) => {
     const response = await User.findOne({ username: req.body.username });
 
     const login_password = req.body.password;
-    const hashed_password = response.password;
+    const hashed_password = response?.password;
+
     if (
-      !response ||
-      (typeof login_password === "string" &&
-        typeof req.body?.username === "string")
+      response != null &&
+      typeof login_password === "string" &&
+      typeof req.body?.username === "string"
     ) {
       const match = bcrypt.compareSync(login_password, hashed_password);
       if (match) {
+        const token = jwt.sign(
+          {
+            username: req.body.username,
+            id: response._id,
+          },
+          process.env.PRIVATE_KEY,
+          {
+            expiresIn: "24h",
+          }
+        );
 
-        
         res.send({
           status: "Success",
           message: "Login successfull",
+          auth_token: token,
         });
       } else {
         res.send({
           status: "Failed",
-          message: "Login Failed",
+          message: "Invalid Login credentials",
         });
       }
     } else {
       res.send({
         status: "Failed",
+        message: "Invalid Login credentials",
       });
     }
   } catch (error) {
     res.status(404).send({
       status: "Failed",
-      message: error,
+      message: error.message || error,
     });
   }
 });
-userRoute.put("/:id", async (req, res) => {
+userRoute.put("/:id", verify_jwt, async (req, res) => {
   try {
-    const existing_user = await User.findOne({username:req.body.username});
-    console.log(existing_user)
+    const existing_user = await User.findOne({ username: req.body.username });
+
     if (existing_user) {
       res.status(400).send({
         status: "Failed",
         message: "username already exists",
       });
     } else {
-        const user = await User.findById(req.params.id);
-        user.username=req.body.username
-        const response = await user.save()
-        console.log(response)
-        if (response){
-            res.status(201).send({
-                status:"success",
-                message:response
-            })
-        }else{
-            res.status(400).send({
-                status:"Failed",
-                message:response
-            })
-        }
+      const user = await User.findById(req.params.id);
+      user.username = req.body.username;
+      const response = await user.save();
+
+      if (response) {
+        res.status(201).send({
+          status: "success",
+          message: response,
+        });
+      } else {
+        res.status(400).send({
+          status: "Failed",
+          message: response,
+        });
+      }
     }
   } catch (error) {
     res.status(500).send({
       status: "Failed",
-      message: error,
+      message: error || error.message,
     });
   }
 });
 
-userRoute.delete("/:id", (req, res) => {
-
+userRoute.delete("/:id", async (req, res) => {
+  try {
+    const response = await User.findById(req.params.id);
+    if (response) {
+      await response.delete();
+      res.send({
+        status: "Success",
+        message: response,
+      });
+    } else {
+      res.send({
+        status: "Failed",
+        message: "Login Failed",
+      });
+    }
+  } catch (error) {
+    res.send({
+      status: "Failed",
+      message: error.message,
+    });
+  }
 });
 module.exports = userRoute;
