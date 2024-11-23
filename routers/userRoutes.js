d780e2ff08b26a4facd614f4f364b8768a90b1ca
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2024-11-23 18:42:06 +0600

    Todo routes added

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -2,4 +2,19 @@ const express = require('express')
 
 const userRoute=express()
 
+userRoute.get('/:id',(req,res)=>{
+
+})
+
+userRoute.post('/:id',(req,res)=>{
+    
+})
+
+userRoute.put('/:id',(req,res)=>{
+    
+})
+
+userRoute.delete('/:id',(req,res)=>{
+    
+})
 module.exports=userRoute
